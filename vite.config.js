f00e395f9cb79b755270df806e0cc84c9b0fb68b
Hariom <suppraaaa2003@gmail.com>
2026-08-18 02:08:45 +0530

    Configure project for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    // Allow serving files from the parent directory (for Assites/)
-    fs: {
-      allow: ['..'],
-    },
-  },
 })
+
